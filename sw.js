f0e3ab90f04d5af18cabd67bd41e8d2315459f3f
sirlucskywalker-hash <sirlucskywalker@gmail.com>
2026-09-21 23:10:53 -0400

    Refresh PWA cache for V32 tab order

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='physiqueos-v31';
-const ASSETS=['./','./index.html','./styles.css?v=31','./data.js?v=31','./app.js?v=31','./manifest.webmanifest'];
+const CACHE='physiqueos-v32';
+const ASSETS=['./','./index.html','./styles.css?v=32','./data.js?v=32','./app.js?v=32','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
