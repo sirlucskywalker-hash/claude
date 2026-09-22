b1fcda855714eea6577a42c1c3134b46c32eb805
sirlucskywalker-hash <sirlucskywalker@gmail.com>
2026-09-21 21:56:09 -0400

    Refresh PWA cache for beta v19

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='physiqueos-v18';
-const ASSETS=['./','./index.html','./styles.css?v=18','./data.js?v=18','./app.js?v=18','./manifest.webmanifest'];
+const CACHE='physiqueos-v19';
+const ASSETS=['./','./index.html','./styles.css?v=19','./data.js?v=19','./app.js?v=19','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
