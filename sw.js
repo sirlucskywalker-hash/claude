f41008a4c62155d58a6781585a58738244aa6bf9
sirlucskywalker-hash <sirlucskywalker@gmail.com>
2026-09-21 23:37:24 -0400

    Refresh PWA cache for meal macro alignment

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='physiqueos-v41';
-const ASSETS=['./','./index.html','./styles.css?v=41','./data.js?v=41','./app.js?v=41','./manifest.webmanifest'];
+const CACHE='physiqueos-v42';
+const ASSETS=['./','./index.html','./styles.css?v=42','./data.js?v=42','./app.js?v=42','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
