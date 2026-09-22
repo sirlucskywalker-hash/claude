370163203cfaef78325d0bcfd9b8fc32eaf6f71c
sirlucskywalker-hash <sirlucskywalker@gmail.com>
2026-09-21 23:21:20 -0400

    Refresh PWA cache for V37 Obsidian

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='physiqueos-v36';
-const ASSETS=['./','./index.html','./styles.css?v=36','./data.js?v=36','./app.js?v=36','./manifest.webmanifest'];
+const CACHE='physiqueos-v37';
+const ASSETS=['./','./index.html','./styles.css?v=37','./data.js?v=37','./app.js?v=37','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
