eb942086a0ccc77b328b3b331433516e5f069c6b
sirlucskywalker-hash <sirlucskywalker@gmail.com>
2026-09-21 21:13:08 -0400

    Refresh PWA cache strategy so beta fixes deploy immediately

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
-const CACHE='physiqueos-v1';const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='physiqueos-v2';
+const ASSETS=['./','./index.html','./styles.css','./data.js','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
