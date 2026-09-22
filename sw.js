6cc470edb8fb6006910d19d75f474187f9ee5a78
sirlucskywalker-hash <sirlucskywalker@gmail.com>
2026-09-21 23:13:20 -0400

    Refresh PWA cache for V33 Apex

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='physiqueos-v32';
-const ASSETS=['./','./index.html','./styles.css?v=32','./data.js?v=32','./app.js?v=32','./manifest.webmanifest'];
+const CACHE='physiqueos-v33';
+const ASSETS=['./','./index.html','./styles.css?v=33','./data.js?v=33','./app.js?v=33','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
