35b03fb14736d674f799641a783b23e14b52f1c1
sirlucskywalker-hash <sirlucskywalker@gmail.com>
2026-09-21 23:27:52 -0400

    Refresh cache for beta v1.1 label

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='physiqueos-v38';
-const ASSETS=['./','./index.html','./styles.css?v=38','./data.js?v=38','./app.js?v=38','./manifest.webmanifest'];
+const CACHE='physiqueos-v39';
+const ASSETS=['./','./index.html','./styles.css?v=39','./data.js?v=39','./app.js?v=39','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
